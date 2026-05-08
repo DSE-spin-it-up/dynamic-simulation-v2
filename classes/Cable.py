54331a8a13e27e43ce28fea9c2a8e44e8cfebde8
Sofia Calvo Morales <sofiacalvomorales@gmail.com>
2026-05-08 16:59:11 +0200

    Base classes for Cable and Drone

---
+++ classes/Cable.py
@@ -0,0 +1,58 @@
+import numpy as np
+
+from Drone import Drone
+
+
+class Cable():
+    def __init__(self, id, length, stiffness, damping, conection_A: Drone, conection_B: Drone):
+        self.id = id
+        self.length = length
+        self.stiffness = stiffness
+        self.damping = damping
+        self.conection_A = conection_A
+        self.conection_B = conection_B
+
+    def __repr__(self):
+        return (
+            f"Cable object, id: {self.id}, length [m]: {self.length}, stiffness [N/m]: {self.stiffness}, damping [N s/m]: {self.damping}\n"
+            f"Assigned nodes\n  n1: {self.conection_A}\n  n2: {self.conection_B}\n"
+        )
+
+    def __relative_pos(self):
+        return np.array([self.conection_A.x - self.conection_B.x])
+
+    def __relative_vel(self):
+        return np.array([self.conection_A.v - self.conection_B.v])
+
+    def __calculate_f_spring(self):
+        relative_pos = self.__relative_pos()
+        norm_pos = np.linalg.norm(relative_pos)
+
+        if norm_pos != 0:
+            unit_vector = relative_pos / norm_pos
+        else:
+            unit_vector = np.array([0, 0, 0])
+
+        f_spring = -self.stiffness * (norm_pos - self.length) * unit_vector
+        return np.squeeze(f_spring)
+
+    def __calculate_f_damping(self):
+        relative_pos = self.__relative_pos()
+        relative_vel = np.squeeze(self.__relative_vel())
+        norm_pos = np.linalg.norm(relative_pos)
+
+        if norm_pos != 0:
+            unit_vector = np.squeeze(relative_pos / norm_pos)
+        else:
+            unit_vector = np.squeeze(np.array([0, 0, 0]))
+
+        f_damping = -self.damping * np.dot(relative_vel, unit_vector) * unit_vector
+        return np.squeeze(f_damping)
+
+    def force_value(self):
+        """Returns force for tensile-only cable (no compression)"""
+        l = np.linalg.norm(self.__relative_pos())
+        if l >= self.length:
+            return self.__calculate_f_spring() + self.__calculate_f_damping()
+        else:
+            return np.array([0, 0, 0])
---
+++ classes/Drone.py
@@ -0,0 +1,42 @@
+import numpy as np
+import numpy.typing as npt
+
+class Drone():
+    def __init__(self, id: int, mass: float, initial_position: np.ndarray, initial_velocity: np.ndarray = np.array([0, 0, 0])):
+        self.id = id
+        self.mass = mass
+        self.position = initial_position
+        self.v = initial_velocity
+
+    # ----------------------------------------------------------------------------------
+    # Ignore this, it's just to print the drone object
+    def __repr__(self):
+        return (
+            f"Drone object, id: {self.id}, position [m]: ({self.x}, {self.y}, {self.z}), mass [kg]: {self.mass}, velocity [m/s]: ({self.vx}, {self.vy}, {self.vz})\n"
+        )
+    
+    # ----------------------------------------------------------------------------------
+    # Also ignore this, is just for easier access to position and velocity components
+    @property
+    def vx(self):
+        return self.v[0]
+
+    @property
+    def vy(self):
+        return self.v[1]
+
+    @property
+    def vz(self):
+        return self.v[2]
+    
+    @property
+    def x(self):
+        return self.position[0]
+    @property
+    def y(self):
+        return self.position[1]
+    @property
+    def z(self):
+        return self.position[2]
+
+    # ----------------------------------------------------------------------------------
